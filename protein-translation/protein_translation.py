61783c664c7003c38758c8526615d23956877fd0
evaitl <evaitl@gmail.com>
2023-06-07 14:46:26 -0400

    protein translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,31 @@
+from itertools import takewhile
+
+XLAT = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP",
+}
+
+
+def proteins(strand):
+    return [
+        p
+        for p in takewhile(
+            lambda p: p!= "STOP",
+            (XLAT[strand[i : i + 3]] for i in range(0, len(strand), 3)),
+        )
+    ]
